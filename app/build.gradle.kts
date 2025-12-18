9f1d7f548fb9779d5cbba38cc9577e500e518ac1
duran <duran.bilen@hotmail.com>
2025-12-18 16:46:27 +0300

    Hatalar giderildi debug ayarları yapıldı

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.workwatch"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.workwatch"
         minSdk = 26
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
